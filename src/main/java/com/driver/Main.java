4ea3a27eeb7d58f705ba616f4db0be1af3abb988
Abhi Pandey <pandeyabhi142002@gmail.com>
2022-12-14 13:24:43 +0530

    Assignment Updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj = new RWOnly();
+
+      obj.setName("Abhi");
+      System.out.println(obj.getName());
+  }
 }
